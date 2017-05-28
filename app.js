85370619e948c8e4918f3bea003f66dc8bd359e2
tejasai97 <teja.g14@gmail.com>
2017-05-28 12:51:29 +0530

    event emitter

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 var counter = require('./count');
 var stuff = require('./stuff');
 var stuff2 = require('./stuff2');
@@ -11,3 +12,28 @@ console.log(stuff.adder(stuff.pi,5));
 console.log(stuff2.counter(['teja','sai','kumar']));
 console.log(stuff2.adder(5,6));
 console.log(stuff2.adder(stuff.pi,5));
+*/
+
+var events =require('events');
+var util=require('util');
+
+var Person = function(name){
+  this.name=name;
+};
+
+util.inherits(Person,events.EventEmitter);
+
+var teja= new Person('teja');
+var mary= new Person('mary');
+var sai= new Person('sai');
+var people=[teja,mary,sai];
+
+people.forEach(function(person){
+  person.on('speak',function(msg){
+    console.log(person.name + ' said: '+ msg);
+  });
+});
+
+teja.emit('speak','hello');
+mary.emit('speak','Im mary');
+sai.emit('speak','ola!');
