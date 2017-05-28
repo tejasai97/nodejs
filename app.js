17ed6e81260c1e3d681ccfeee205e12ef5d8ebe5
tejasai97 <teja.g14@gmail.com>
2017-05-28 12:30:28 +0530

    module patterns

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 var counter = require('./count');
+var stuff = require('./stuff');
+var stuff2 = require('./stuff2');
 
 console.log(counter(['teja','sai','kumar']));
+
+console.log(stuff.counter(['teja','sai','kumar']));
+console.log(stuff.adder(5,6));
+console.log(stuff.adder(stuff.pi,5));
+
+console.log(stuff2.counter(['teja','sai','kumar']));
+console.log(stuff2.adder(5,6));
+console.log(stuff2.adder(stuff.pi,5));
