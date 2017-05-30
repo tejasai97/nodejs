651b7d24b8831585ab4f5b3308bc4467e02c1321
tejasai97 <teja.g14@gmail.com>
2017-05-30 17:48:45 +0530

    Writable Streams

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var http =require('http');
 var fs=require('fs');
 
 var myReadStream = fs.createReadStream(__dirname + '/readme.txt','utf8');
+var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt');
 
 myReadStream.on('data',function(chunk){
   console.log('new chunk received:');
-  console.log(chunk);
+  myWriteStream.write(chunk);
 });
 
 
