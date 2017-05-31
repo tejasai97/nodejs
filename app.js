825e0787f3f7bff69e1a8f1317073542051f5f53
tejasai97 <teja.g14@gmail.com>
2017-05-31 11:13:37 +0530

    pipes
    createing pipes and displaying data on server using pipes

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 var http =require('http');
 var fs=require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '/readme.txt','utf8');
-var myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt');
-
-myReadStream.on('data',function(chunk){
-  console.log('new chunk received:');
-  myWriteStream.write(chunk);
-});
 
 
 
-/*
+
+
 var server = http.createServer(function(req,res){
   console.log('request was made:' +req.url);
   res.writeHead(200,{'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  var myReadStream = fs.createReadStream(__dirname + '/readme.txt','utf8');
+  myReadStream.pipe(res);
 });
 
 server.listen(3000, '127.0.0.1');
 console.log('now listening to port 3000');
-*/
