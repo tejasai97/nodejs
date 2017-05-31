0e62bb56e60446a06653f2a441c1ecdaaf90a8bd
tejasai97 <teja.g14@gmail.com>
2017-05-31 20:14:07 +0530

    Serving JSON Data

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ var fs=require('fs');
 
 var server = http.createServer(function(req,res){
   console.log('request was made:' +req.url);
-  res.writeHead(200,{'Content-Type': 'text/html'});
-  var myReadStream = fs.createReadStream(__dirname + '/index.html','utf8');
-  myReadStream.pipe(res);
+  res.writeHead(200,{'Content-Type': 'application/json'});
+  var myObj = {
+    name : 'teja',
+    job : 'programmer',
+    age : 20
+  };
+  res.end(JSON.stringify(myObj));
 });
 
 server.listen(3000, '127.0.0.1');
