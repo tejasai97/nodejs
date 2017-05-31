be66cd162594dd6cfc7df92a5295709e712f7115
tejasai97 <teja.g14@gmail.com>
2017-05-31 20:30:58 +0530

    Basic Routing

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ var fs=require('fs');
 
 var server = http.createServer(function(req,res){
   console.log('request was made:' +req.url);
-  res.writeHead(200,{'Content-Type': 'application/json'});
-  var myObj = {
-    name : 'teja',
-    job : 'programmer',
-    age : 20
-  };
-  res.end(JSON.stringify(myObj));
+  if(req.url === '/home' || req.url === '/'){
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
+  }
+  else if(req.url === '/contact'){
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/contact.html').pipe(res);
+  }
+  else if(req.url === '/api/data'){
+    var data = [{name : 'teja',age:20},{name :'sai',age:25}];
+    res.writeHead(200,{'Content-Type' : 'application/json'});
+    res.end(JSON.stringify(data));
+  }
+  else{
+    res.writeHead(404,{'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/404.html').pipe(res);
+  }
 });
 
 server.listen(3000, '127.0.0.1');
