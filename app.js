3ada62497ef3ea00ee83ac780146d0aed1fa746c
tejasai97 <teja.g14@gmail.com>
2017-05-31 11:20:47 +0530

    Serving HTML Pages

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 var http =require('http');
 var fs=require('fs');
 
-
-
-
-
-
 var server = http.createServer(function(req,res){
   console.log('request was made:' +req.url);
-  res.writeHead(200,{'Content-Type': 'text/plain'});
-  var myReadStream = fs.createReadStream(__dirname + '/readme.txt','utf8');
+  res.writeHead(200,{'Content-Type': 'text/html'});
+  var myReadStream = fs.createReadStream(__dirname + '/index.html','utf8');
   myReadStream.pipe(res);
 });
 
