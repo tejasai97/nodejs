047d725bb157f455e481a28a7ec8e3e864a96ee8
tejasai97 <teja.g14@gmail.com>
2017-06-01 16:57:40 +0530

    Handling POST request

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
-
+var bodyParser = require('body-parser');
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.set('view engine','ejs');
 
@@ -9,6 +11,10 @@ app.use('/assets',express.static('assets'));
 
 app.get('/',function(req,res){
   res.render('index');
+});
+
+app.post('/contact',urlencodedParser, function (req, res) {
+  res.render('contact-sucess',{data:req.body});
 });
 
 app.get('/contact',function(req,res){
