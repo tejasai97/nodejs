547cb196de5dd4fdc1420260fded4fcb58303b50
tejasai97 <teja.g14@gmail.com>
2017-06-01 11:16:23 +0530

    Express Route Params

--- app.js
+++ app.js
@@ -10,5 +10,9 @@ app.get('/contact',function(req,res){
   res.send('this is contact page');
 });
 
+app.get('/profile/:id',function(req,res){
+  res.send('You requested to see a profile with id of : '+ req.params.id);
+});
+
 
 app.listen(3000);
